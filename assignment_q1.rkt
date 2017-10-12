6bc864b3f263d7e3be28056433406146e81fa6ec
Andrew Reid East <andrew@andreweast.net>
2017-10-12 14:30:06 +0100

    Q1 Lisp code

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,6 @@
 #lang racket
+(cons 46 2)
+(cons 3 (cons 2 (cons 1 empty)))
+(cons "string" (cons 42 (cons (cons 5 (cons 8 (cons 13 empty))) '())))
+(list "thread" 1 (list 1 2 3))
+(append '("yarn") '(1) '((1 2 3)))
