c3c2fc3b4747ce70c5f7388633a083ccd12f0873
Andrew Reid East <andrew@andreweast.net>
2017-10-19 00:15:37 +0100

    Working on count_instances

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -17,20 +17,30 @@
 
 (define (ins_beg el lst)
   (append (cons el '()) lst))
+(ins_beg 'a '(b c d))
+(ins_beg '(a b) '(b c d))
 
 (define (ins_end el lst)
   (append lst (list el)))
+(ins_end 'a '(b c d))
+(ins_end '(a b) '(b c d))
 
 (define (cout_top_level lst)
   (if (empty? lst)
       0
       (+ 1 (cout_top_level (cdr lst)))))
-
-(ins_beg 'a '(b c d))
-(ins_beg '(a b) '(b c d))
-
-(ins_end 'a '(b c d))
-(ins_end '(a b) '(b c d))
-
 (cout_top_level (ins_beg 'a '(b c d)))
 (cout_top_level (ins_end 'a '(b c d e)))
+
+(define (count_instances item lst)
+  (cond
+    [(empty? lst) 0]
+    [(cond
+       [(equal? item (car lst)) (+ 1 (count_instances item (cdr lst)))]
+       (else (count_instances item (cdr lst))))]))
+(count_instances 'a '(a b c d))
+(count_instances 'a '(a b a c d))
+(count_instances 'a '(b c d))
+(count_instances 'a '(a a a a a))
+(count_instances '(a b) '((a b) (a c) d))
+(count_instances '(a b) '((a b) (a c) d (a b) (b a) (u u d d l r l r b a)))
