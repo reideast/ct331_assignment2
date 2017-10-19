d3b6fdb1e5abc6972315bd3b74f644b4032113be
Andrew Reid East <andrew@andreweast.net>
2017-10-19 15:13:57 +0100

    Begin q3

--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -1,1 +1,7 @@
 #lang racket
+
+(define i_am_root '(() 3 ()))
+(define two_tree '((() 1 ()) 2 (() 3 ())))
+(define bigger_tree '(((() 3 ()) 5 (() 7 ())) 10 (() 15 ())))
+(define complete_tree '(((() 3 ()) 5 (() 7 ())) 10 ((() 13 ()) 15 (() 17 ()))))
+
