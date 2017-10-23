38238b6871b3e966ba50cb9522c72f7fc853f2c7
Andrew Reid East <andrew@andreweast.net>
2017-10-23 10:42:56 +0100

    Tree: Working tree traversal recursive function

--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -5,3 +5,21 @@
 (define bigger_tree '(((() 3 ()) 5 (() 7 ())) 10 (() 15 ())))
 (define complete_tree '(((() 3 ()) 5 (() 7 ())) 10 ((() 13 ()) 15 (() 17 ()))))
 
+(define (traverse tree callback)
+  (begin
+    (printf "at tree=~a~n" tree)
+  (cond
+    [(equal? tree '())]
+    [else (begin
+       (traverse (car tree) callback)
+       (callback (cadr tree))
+       (traverse (caddr tree) callback)
+       )])
+    (printf "done with tree=~a~n" tree)
+    ))
+
+(display "Traverse & Print a tree with only root\n")
+(traverse i_am_root (curry fprintf (current-output-port) "~a~n"))
+
+(display "Traverse & Print a complete tree with height 2\n")
+(traverse two_tree writeln)
