eb4e4d761d01bce77fda04966a26393abadcce5f
Andrew Reid East <andrew@andreweast.net>
2017-10-26 13:46:34 +0100

    Implement inserting a single item into a tree

--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -45,3 +45,27 @@
 (binary_search complete_tree 17)
 (display "Search large tree for value that does not exists (should be #f): ")
 (binary_search complete_tree 256)
+
+; Part C: Insert a value into a BST
+(define (insert_item tree item)
+;  (begin (printf "Inserting into tree=~a key=~a~n" tree item)
+  (cond
+    [(null? tree) (list '() item '())]
+    [(equal? item (cadr tree)) tree]
+    [(< item (cadr tree)) (list (insert_item (car tree) item) (cadr tree) (caddr tree))]
+    [(> item (cadr tree)) (list (car tree) (cadr tree) (insert_item (caddr tree) item))]))
+(display "Insert 4 into an empty/null tree. Should be (() 4 ()): ")
+(insert_item '() 4)
+(display "Insert 3 into a small tree where it already exists, should be (() 3 ()): ")
+(insert_item i_am_root 3)
+(display "Insert 15 into a bigger tree where it already exists, should be '(((() 3 ()) 5 (() 7 ())) 10 (() 15 ()))): ")
+(insert_item bigger_tree 3)
+(display "Insert 5 into the example tree, should be '(((() 1 ()) 3 ((() 4 (() 5 ())) 6 (() 7 ()))) 8 (() 10 ((() 13 ()) 14 ()))): ")
+(define updated_tree (insert_item example_tree 5)) ;save the results
+updated_tree ;then, display the results
+(display "Insert 9 into the result of the previous, should be '(((() 1 ()) 3 ((() 4 (() 5 ())) 6 (() 7 ()))) 8 ((() 9 ()) 10 ((() 13 ()) 14 ()))): ")
+(insert_item updated_tree 9)
+(display "Insert first 2, then 1, then 3 into a null tree. Should be ((() 1 ()) 2 (() 3 ())): ");
+(insert_item (insert_item (insert_item '() 2) 1) 3)
+
+; Part D: Insert a list into a BST
