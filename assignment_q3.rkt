41fc952ce3f550aa98888941ab81cc7822c8a782
Andrew Reid East <andrew@andreweast.net>
2017-10-26 13:46:14 +0100

    Update some of the basic definitions, previous tests

--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -1,11 +1,17 @@
 #lang racket
 (provide traverse)
+(provide binary_search)
+(provide insert_item)
+;(provide insert_list)
+;(provide treesort)
+;(provide treesort_compare)
 
 ; Define some binary search trees
 (define i_am_root '(() 3 ()))
 (define two_tree '((() 1 ()) 2 (() 3 ())))
 (define bigger_tree '(((() 3 ()) 5 (() 7 ())) 10 (() 15 ())))
 (define complete_tree '(((() 3 ()) 5 (() 7 ())) 10 ((() 13 ()) 15 (() 17 ()))))
+(define example_tree '(((() 1 ()) 3 ((() 4 ()) 6 (() 7 ()))) 8 (() 10 ((() 13 ()) 14 ()))))
 
 ; Part A: Display the contents of a binary search tree (in sorted order)
 (define (traverse tree callback)
@@ -14,20 +20,22 @@
        (traverse (car tree) callback)
        (callback (cadr tree))
        (traverse (caddr tree) callback))]))
-(display "Traverse & Print a tree with only root\n")
-(traverse i_am_root (curry fprintf (current-output-port) "~a~n"))
-(display "Traverse & Print a complete tree with height 2\n")
-(traverse two_tree writeln)
+(display "Traverse & Print a tree with only root: ")
+(traverse i_am_root write)
+(display "\nTraverse & Print a complete tree with height two: ")
+(traverse two_tree (curry printf "~a,"))
+(display "\nTraverse & Print the example tree: ")
+(define traverse_print (curryr traverse (curry printf "~a,"))) ;curryr is like curry, but does arguments right-to-left
+(traverse_print example_tree)
 
 ; Part B: Search the tree for a value
 (define (binary_search tree key)
-;  (begin (printf "Searching tree=~a key=~a~n" tree key)
   (cond
     [(null? tree) #f]
     [(equal? key (cadr tree)) #t]
     [(< key (cadr tree)) (binary_search (car tree) key)]
     [(> key (cadr tree)) (binary_search (caddr tree) key)]))
-(display "Search single item tree for value that exists (should be #t): ")
+(display "\nSearch single item tree for value that exists (should be #t): ")
 (binary_search i_am_root 3)
 (display "Search single item tree for value that does not exists (should be #f): ")
 (binary_search i_am_root 4)
